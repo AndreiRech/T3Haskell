37532ec4c31862b2c60a07a4a4de0a6a58426c1d
Andrei Rech <136367100+AndreiRech@users.noreply.github.com>
2024-11-11 21:35:08 -0300

    novas funcoes

--- Trabalho.hs
+++ Trabalho.hs
@@ -1,4 +1,6 @@
-data Item = Item { nome :: String, preco :: Int } deriving Show
+import System.Random (randomRIO)
+
+data Item = Item { nome :: String, preco :: Int, importancia :: Int } deriving Show
 
 main :: IO ()
 main = do
@@ -13,14 +15,18 @@ menu lista = do
   putStrLn "2. Listar todos os itens no kit"
   putStrLn "3. Remover item do kit"
   putStrLn "4. Calcular custo total do kit"
-  putStrLn "5. Sair da aplicação"
+  putStrLn "5. Avaliar o kit"
+  putStrLn "6. Testar o kit"
+  putStrLn "7. Sair da aplicação"
   opc <- getLine
   case opc of
     "1" -> adicionarItem lista
     "2" -> listarItens lista
     "3" -> removerItem lista
     "4" -> calculaCusto lista
-    "5" -> putStrLn "Saindo..."
+    "5" -> avaliaKit lista
+    "6" -> testaKit lista
+    "7" -> putStrLn "Saindo..."
     _   -> putStrLn "Insira uma opção válida." >> menu lista
 
 
@@ -33,8 +39,11 @@ adicionarItem lista = do
     
   putStrLn "Digite o preço do item: "
   precoItem <- readLn
+
+  putStrLn "Digite a importância desse item: "
+  importanciaItem <- readLn
   
-  let item = Item nomeItem precoItem
+  let item = Item nomeItem precoItem importanciaItem
   let nova = lista ++ [item]
   
   putStrLn $ "O item " ++ nomeItem ++ " foi adicionado ao kit."
@@ -54,8 +63,8 @@ listarItens lista = do
 
 imprimirItens :: [Item] -> IO ()
 imprimirItens [] = return ()
-imprimirItens (Item nome preco:xs) = do
-  putStrLn $ "- " ++ nome ++ ": R$" ++ show preco
+imprimirItens (Item nome preco importancia:xs) = do
+  putStrLn $ "- " ++ nome ++ ": R$" ++ show preco ++ " : Importancia - " ++ show importancia
   imprimirItens xs
   
 
@@ -84,7 +93,7 @@ removeNome _ [] = []
 removeNome nome (x:xs)
   | nome == item x = xs
   | otherwise = x : removeNome nome xs
-  where item (Item nome _) = nome
+  where item (Item nome _ _) = nome
   
   
 -- Função para Calcular custo dos Itens
@@ -97,3 +106,46 @@ calculaCusto lista = do
     let custoTotal = sum (map preco lista)
     putStrLn $ "O custo total do kit é: R$ " ++ show custoTotal
   menu lista
+
+
+-- Função Especial - Avaliar o Kit
+
+avaliaKit :: [Item] -> IO ()
+avaliaKit lista = do
+  if null lista
+    then putStrLn "O kit está vazio."
+    else do
+      let mediaImportancia = calculaMediaImportancia lista
+
+          mensagem = case mediaImportancia of
+            _ | mediaImportancia < 4 -> "Reprovado: O kit é muito fraco."
+              | mediaImportancia <= 7 -> "Atenção: O kit pode melhorar..."
+              | otherwise -> "Aprovado: O kit está bem preparado!"
+
+      putStrLn $ "A média de importância dos itens é: " ++ show mediaImportancia
+      putStrLn mensagem
+  menu lista
+
+-- Função Especial - Testar o Kit
+
+testaKit :: [Item] -> IO ()
+testaKit lista = do
+  if null lista
+    then putStrLn "O kit está vazio."
+    else do
+      let mediaImportancia = calculaMediaImportancia lista
+
+      valorAleatorio <- randomRIO (1, 20) :: IO Double
+      let resultado = mediaImportancia + valorAleatorio
+      putStrLn $ "Resultado do teste: " ++ show resultado
+
+      if resultado > 17
+        then putStrLn "Parabéns! Você conseguiu sobreviver a nossa simulação com o kit!"
+        else putStrLn "Não deu. O kit não foi suficiente para sobreviver a simulação. Recomendamos melhorar ele."
+  menu lista
+
+calculaMediaImportancia :: [Item] -> Double
+calculaMediaImportancia lista = 
+  let somaImportancia = sum (map importancia lista)
+      quantidadeItens = length lista
+  in fromIntegral somaImportancia / fromIntegral quantidadeItens
